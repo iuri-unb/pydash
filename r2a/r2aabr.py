a9a6497b095543c48fc0ea1c4eb84851f5216fcf
Iuri <160152488@aluno.unb.br>
2020-11-24 09:45:14 -0300

    Adding some changes

---
+++ r2a/r2aabr.py
@@ -0,0 +1,40 @@
+import time
+
+from player.parser import *
+from r2a.ir2a import IR2A
+
+class R2AAbr(IR2A):
+
+    def __init__(self, id):
+        IR2A.__init__(self, id)
+        self.parsed_mpd = ''
+        self.qi = []
+
+    def handle_xml_request(self, msg):        
+        self.send_down(msg)
+
+    def handle_xml_response(self, msg):
+        # getting qi list
+        self.parsed_mpd = parse_mpd(msg.get_payload())
+        self.qi = self.parsed_mpd.get_qi()
+
+        self.send_up(msg)
+
+    def handle_segment_size_request(self, msg):
+        # time to define the segment quality choose to make the request
+        inicio = time.time();
+        
+        msg.add_quality_id(self.qi[0])
+        self.send_down(msg)
+
+    def handle_segment_size_response(self, msg):
+        fim = time.time();
+        transfer_t = (fim - inicio);
+        print(transfer_t);
+        self.send_up(msg)
+
+    def initialize(self):
+        pass
+
+    def finalization(self):
+        pass
